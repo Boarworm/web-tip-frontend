47c2a91131a5e666b4e769cccd67b3f9d6393872
boarworm <klepaysayt@gmail.com>
2025-07-05 14:00:42 +0200

    Change interface name

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,12 +1,12 @@
 import { ReactNode } from "react"
 import { cn } from "@/lib/utils"
 
-interface ContainerProps {
+interface Props {
   children: ReactNode
   className?: string
 }
 
-const Container = ({ children, className = "" }: ContainerProps) => {
+const Container = ({ children, className = "" }: Props) => {
   return (
     <div className={cn("w-full max-w-[1440px] mx-auto px-4 xl:px-12", className)}>
       {children}
