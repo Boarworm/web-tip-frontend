16f1bdfc6c216930040787c966199080b220a895
boarworm <klepaysayt@gmail.com>
2025-07-06 18:53:51 +0200

    Create Logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import { cn } from '@/lib/utils'
+
+interface Props {
+  className?: string
+  color?: 'dark' | 'light'
+  logoSrc?: string
+  title?: string
+  text?: string
+  showText?: boolean
+}
+
+export default function Logo(
+  {
+    className = "",
+    color = "light",
+    logoSrc = "/images/logo-light.svg",
+    title = "WTT",
+    text = "WTT",
+    showText = true
+  }: Props
+) {
+  const textColorClass = color === 'dark' ? 'text-gray-900' : 'text-white'
+
+  return (
+    <div className={cn("flex", className)}>
+      <Link href="/" className="inline-flex items-center">
+        <img
+          src={logoSrc}
+          alt={title}
+          title={title}
+          className="w-[35px] md:w-[46px]"
+        />
+        {showText && (
+          <span className={`relative ${textColorClass} leading-none font-light text-4xl ml-3 lg:text-5xl transition-colors duration-200`}>
+            {text}
+          </span>
+        )}
+      </Link>
+    </div>
+  )
+}
