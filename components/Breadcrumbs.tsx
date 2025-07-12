6f28c2265b88ef3a709cec4dbe226c78caf605d6
boarworm <klepaysayt@gmail.com>
2025-07-12 20:49:39 +0200

    Fix centering

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -14,49 +14,51 @@ interface Props {
 
 export default function Breadcrumbs({ breadcrumbs, className }: Props) {
   return (
-    <ol className={cn("flex items-center justify-start overflow-y-hidden overflow-x-auto p-0 list-none", className)} itemScope itemType="https://schema.org/BreadcrumbList">
-      {/* Home breadcrumb */}
-      <li itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
-        <Link href="/" itemProp="item" className="flex items-center text-sm no-underline transition text-white hover:underline">
-          <div itemProp="name">Home</div>
-        </Link>
-        <meta itemProp="position" content="1"/>
-      </li>
+    <div className="flex justify-center">
+      <ol className={cn("flex items-center justify-start overflow-y-hidden overflow-x-auto p-0 list-none", className)} itemScope itemType="https://schema.org/BreadcrumbList">
+        {/* Home breadcrumb */}
+        <li itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
+          <Link href="/" itemProp="item" className="flex items-center text-sm no-underline transition text-white hover:underline">
+            <div itemProp="name">Home</div>
+          </Link>
+          <meta itemProp="position" content="1"/>
+        </li>
 
-      {/* Separator after Home */}
-      <li className="px-2">
-        <span className="text-white">/</span>
-      </li>
+        {/* Separator after Home */}
+        <li className="px-2">
+          <span className="text-white">/</span>
+        </li>
 
-      {/* Dynamic breadcrumbs */}
-      {breadcrumbs.map((breadcrumb, index) => {
-        const position = index + 2 // +2 because Home is position 1
-        const isLast = index === breadcrumbs.length - 1
+        {/* Dynamic breadcrumbs */}
+        {breadcrumbs.map((breadcrumb, index) => {
+          const position = index + 2 // +2 because Home is position 1
+          const isLast = index === breadcrumbs.length - 1
 
-        return (
-          <React.Fragment key={breadcrumb.url || breadcrumb.name}>
-            <li itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
-              {isLast ? (
-                <div className="whitespace-nowrap text-white text-sm" itemProp="name">
-                  {breadcrumb.name}
-                </div>
-              ) : (
-                <Link className="text-white text-sm whitespace-nowrap no-underline transition hover:underline" href={breadcrumb.url} itemProp="item">
-                  <div itemProp="name">{breadcrumb.name}</div>
-                </Link>
-              )}
-              <meta itemProp="position" content={position.toString()}/>
-            </li>
-
-            {/* Separator (not for last item) */}
-            {!isLast && (
-              <li className="px-2">
-                <span className="text-white">/</span>
+          return (
+            <React.Fragment key={breadcrumb.url || breadcrumb.name}>
+              <li itemProp="itemListElement" itemScope itemType="https://schema.org/ListItem">
+                {isLast ? (
+                  <div className="whitespace-nowrap text-white text-sm" itemProp="name">
+                    {breadcrumb.name}
+                  </div>
+                ) : (
+                  <Link className="text-white text-sm whitespace-nowrap no-underline transition hover:underline" href={breadcrumb.url} itemProp="item">
+                    <div itemProp="name">{breadcrumb.name}</div>
+                  </Link>
+                )}
+                <meta itemProp="position" content={position.toString()}/>
               </li>
-            )}
-          </React.Fragment>
-        )
-      })}
-    </ol>
+
+              {/* Separator (not for last item) */}
+              {!isLast && (
+                <li className="px-2">
+                  <span className="text-white">/</span>
+                </li>
+              )}
+            </React.Fragment>
+          )
+        })}
+      </ol>
+    </div>
   )
 }
