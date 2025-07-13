c42a0a5f184257f4580205ea8c1fd691cec14f86
boarworm <klepaysayt@gmail.com>
2025-07-13 13:03:40 +0200

    Create CTA component

---
+++ components/Cta.tsx
@@ -0,0 +1,18 @@
+import { BackgroundLines } from "@/components/ui/BackgroundLines"
+
+export default function Cta() {
+  return (
+    <BackgroundLines className="relative flex items-center justify-center w-full flex-col px-4">
+      <div className="z-10">
+        <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+          Contact me today!
+        </h2>
+        <div className="prose">
+          <p className="max-w-xl mx-auto text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center">
+            <a className="text-4xl" href="mailto:info@web-tip.top">info@web-tip.top</a>
+          </p>
+        </div>
+      </div>
+    </BackgroundLines>
+  )
+}
